c89057653cf644ff466903b6f39a669c842d29db
kamanamsaipk <131681786+kamanamsaipk@users.noreply.github.com>
2023-05-12 20:25:29 +0530

    Update mini_dictionary.py

--- mini_dictionary.py
+++ mini_dictionary.py
@@ -1,7 +1,7 @@
 dict1 = {}
 for ascii_value in range(97, 123):
     dict1[chr(ascii_value)] = ascii_value     
-print("created dictory is :", dict1)
+print("created dictionary is :", dict1)
     
 
-#created dictory is : {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122}
+#created dictionary is : {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122}
